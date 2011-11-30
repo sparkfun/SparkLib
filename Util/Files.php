5391c0474157e374848e9972ac5d7822a7ce6b36
Brennen Bearnes <brennen@sparkfun.com>
2011-11-30 10:58:15 -0700

    slightly more consistent code formatting

--- Util/Files.php
+++ Util/Files.php
@@ -3,21 +3,23 @@ namespace SparkLib\Util;
 
 class Files {
 
-  public static function scandir_by_ctime($dir, $direction=null) {
+  public static function scandir_by_ctime ($dir, $direction = null)
+  {
     if('ASC' != $direction)
       $direction = null;
 
     $filez = scandir($dir);
     $sort = array();
-    foreach($filez as $file) {
+    foreach ($filez as $file) {
       if ($file != '.' && $file != '..') {
-        if (filectime($dir . $file) === false) return false;
-        $date = date("YmdHis", filectime($dir.$file));
+        if (filectime($dir . $file) === false)
+          return false;
+        $date = date("YmdHis", filectime($dir . $file));
         $sort[$date] = $file;
       }
     }
 
-    if('ASC' == $direction)
+    if ('ASC' == $direction)
       ksort($sort);
     else
       krsort($sort);
@@ -25,7 +27,8 @@ class Files {
     return $sort;
   }
 
-  public static function is_empty_dir($dir){
+  public static function is_empty_dir ($dir)
+  {
      return (($files = @scandir($dir)) && count($files) <= 2);
   }
 
