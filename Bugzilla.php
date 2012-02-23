abc369d8e61a2e7d8bcee2a5cd4eafd82b3c9bfc
Brennen Bearnes <brennen@sparkfun.com>
2012-02-23 16:12:18 -0700

    catch and handle XML_RPC2 exceptions

--- Bugzilla.php
+++ Bugzilla.php
@@ -26,15 +26,23 @@ class Bugzilla {
    * Return an object representing a single bug.
    *
    * @param $id integer bug id
+   * @return stdClass bug for given id
+   * @return false if no such bug found
    */
   public function bug ($id)
   {
     $options = array('prefix' => 'Bug.');
 
     $client = \XML_RPC2_Client::create($this->_uri, $options);
-    $result = $client->get(
-      array('ids' => array($id)
-    ));
+
+    try {
+      $result = $client->get(
+        array('ids' => array($id)
+      ));
+    } catch (\XML_RPC2_FaultException $e) {
+      Fail::log('bug not found: ' . $e->getFaultCode() . ' ' . $e->getFaultString());
+      return false;
+    }
 
     // send back a stdClass for the first bug in the results
     return (object)($result['bugs'][0]);
@@ -46,12 +54,20 @@ class Bugzilla {
    * See: http://www.bugzilla.org/docs/4.2/en/html/api/Bugzilla/WebService/Bug.html#search
    *
    * @param $params array of search fields => values
+   * @return array of stdClass bug objects for given search
+   * @return false if search failed altogether (I think)
    */
   public function search (array $params)
   {
     $options = array('prefix' => 'Bug.');
     $client = \XML_RPC2_Client::create($this->_uri, $options);
-    $result = $client->search($params);
+
+    try {
+      $result = $client->search($params);
+    } catch (\XML_RPC2_FaultException $e) {
+      Fail::log('search failed: ' . $e->getFaultCode() . ' ' . $e->getFaultString());
+      return false;
+    }
 
     $bugs = array();
     foreach ($result['bugs'] as $bug) {
--- README.md
+++ README.md
@@ -11,4 +11,14 @@ of the language. Cleverness is avoided where possible.
 
 Documentation, such as it is, can generally be found in the code.
 
+Contents include:
+
+* Autoloader - a simple, almost-standards-compliant autoloader
+* Bugzilla - a very simple client for the Bugzilla API as exposed via XML-RPC
+* Fail - a simple error and exception logging facility
+* Math - math stuffs
+* SocialNoise - quick and dirty display of search results for some social networks
+* Template - a simple interface for using PHP files as templates
+* Util - a miscellany
+
 -- BPB
