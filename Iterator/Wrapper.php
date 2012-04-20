f288171aac54e293b4577c9e3141a86dc43ad30c
Brennen Bearnes <brennen@sparkfun.com>
2012-04-20 13:13:47 -0600

    Iterator and Iterator\Wrapper
    A base class for SPL Iterators. Adds an each(), a weak sort of map(), and
    the expectation of a getNext().
    
    \SparkLib\Iterator\Wrapper is a child class that takes another iterator
    and wraps its result by instantiating a given class. At the moment, we're
    using it to turn arrays from Mongo into instances of a model class.

---
+++ Iterator/Wrapper.php
@@ -0,0 +1,42 @@
+<?php
+namespace SparkLib\Iterator;
+
+class Wrapper extends \SparkLib\Iterator {
+
+  protected $_iterator;
+  protected $_wrapperClass;
+
+  public function __construct (\Iterator $iterator, $wrapper_class = '\stdClass')
+  {
+    $this->_iterator = $iterator;
+    $this->_wrapperClass = $wrapper_class;
+  }
+
+  public function current ()
+  {
+    return $this->makeInstance($this->_iterator->current());
+  }
+
+  // not sure this belongs here, but...
+  public function getNext ()
+  {
+    return $this->makeInstance($this->_iterator->getNext());
+  }
+
+  protected function makeInstance ($result)
+  {
+    $wclass = $this->_wrapperClass;
+    return new $wclass($result);
+  }
+
+  public function key    () { return $this->_iterator->key();    }
+  public function next   () { return $this->_iterator->next();   }
+  public function rewind () { return $this->_iterator->rewind(); }
+  public function valid  () { return $this->_iterator->valid();  }
+
+  public function __call ($name, $arguments)
+  {
+    return call_user_func(array($this->_iterator, $name), $arguments);
+  }
+
+}
--- README.md
+++ README.md
@@ -14,8 +14,9 @@ Documentation, such as it is, can generally be found in the code.
 Contents include:
 
 * Autoloader - a simple, almost-standards-compliant autoloader
-* Bugzilla - a very simple client for the Bugzilla API as exposed via XML-RPC
+* Bugzilla - a very simple client for a subset of the Bugzilla API
 * Fail - a simple error and exception logging facility
+* Iterator - A base class with extra methods for SPL Iterators
 * Math - math stuffs
 * SocialNoise - quick and dirty display of search results for some social networks
 * Template - a simple interface for using PHP files as templates
