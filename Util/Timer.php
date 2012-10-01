198e5d75bdc9df1760dcf6079cba0b87f94deb16
Casey Dentinger <caseyd@sparkfun.com>
2012-10-01 15:35:43 -0600

    adding exec timer utility

---
+++ Util/Timer.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace SparkLib\Util;
+
+class Timer {
+
+  private $_startTime = null;
+
+  public function __construct ()
+  {
+    $this->reset();
+  }
+
+  public function reset ()
+  {
+    $time = microtime();
+    $time = explode(' ', $time);
+    $this->_startTime = $time[1] + $time[0];
+  }
+
+  public function spent ()
+  {
+    $time = microtime();
+    $time = explode(' ', $time);
+
+    return round(($time[1] + $time[0]) - $this->_startTime, 2);
+  }
+
+}
