f52d8cdada89d1d7f3b0796a09241e99b640d18e
Robert L. Carpenter <robacarp@gmail.com>
2013-02-26 13:53:05 -0700

    a few other attribs on an address.

--- Shipping/Address.php
+++ Shipping/Address.php
@@ -9,12 +9,14 @@ class Address {
   // Strings
   public $first_name;
   public $last_name;
+  public $company;
   public $address;
   public $address2;
   public $city;
   public $state;
   public $postal_code;
   public $country;
+  public $phone_number;
 
   public function isDomestic(){
     return !! $this->domestic;
